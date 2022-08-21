daa5300e639c7d223b30fbd980721b804cdeb53b
junhyeok.lee <junhyeok.lee@moreh.io>
2022-08-21 23:11:22 +0900

    logging exp hash

--- model.py
+++ model.py
@@ -1,16 +1,25 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
 import logging
+import git
 
 
 def lam(x, multiplier):
     return x ** multiplier
+
+def log_git_hash()-> str:
+	g = git.Git('./')
+	hash = g.log().split('\n')[0].split(' ')[1]
+	log = logging.getLogger(__name__)
+	log.info(hash)
+	return hash
 
 
 @hydra.main(version_base=None, config_path="config", config_name="cfg")
 def main(cfg: DictConfig) -> None:
     log = logging.getLogger(__name__)
     log.info(cfg.host_name)
+    log_git_hash()
     # for x in range(10):
     #     logger.info(lam(x))
 
