a8bfdefaa859fdeb3535bc41739be2d430835917
junhyeok.lee <junhyeok.lee@moreh.io>
2022-08-21 22:29:11 +0900

    add config and output dvc

--- model.py
+++ model.py
@@ -1,11 +1,21 @@
-from loguru import logger
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import logging
 
-def lam(x):
-    return x ** 2
 
+def lam(x, multiplier):
+    return x ** multiplier
+
+
+@hydra.main(version_base=None, config_path="config", config_name="cfg")
+def main(cfg: DictConfig) -> None:
+    log = logging.getLogger(__name__)
+    log.info(cfg.host_name)
+    # for x in range(10):
+    #     logger.info(lam(x))
 
 if __name__ == '__main__':
-    for x in range(10):
-        logger.info(lam(x))
+    main()
+    
 
     
